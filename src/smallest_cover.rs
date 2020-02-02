8fb5d467db3e6d2c4347120edf4be0e2ae7ed5cd
CrepeGoat <awqatty.b@gmail.com>
2020-02-02 15:44:51 -0500

    changed interface `PointCloud` -> `MovingPointCloud`
    problems with old method:
    - cannot guarantee that floating-point errors from external point
      generation will not occur
    -> cannot reliably remove floating-points (like `Point`s) *by value*
       from a set
    
    solution:
    - changed internally to convert `Point`s to raw bits first
    - changed to `MovingPointCloud`, which tracks points internally with
      `point_log` s.t. identical items are always used to push & pop
      -> can effectively hash floats w/o consequence

--- src/smallest_cover.rs
+++ src/smallest_cover.rs
@@ -3,18 +3,21 @@ use crate::polygon::ConvexPolygon;
 
 use std::iter::Iterator;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 
-pub struct PointCloud{
+pub struct MovingPointCloud{
 	cover: ConvexPolygon,
-	interior: HashSet<Point>,
+	interior: HashSet<(u64, u64)>,
+	point_log: VecDeque<Point>,
 }
 
-impl PointCloud {
+impl MovingPointCloud {
 	pub fn new() -> Self {
 		Self{
 			cover: ConvexPolygon::new(),
 			interior: HashSet::new(),
+			point_log: VecDeque::new(),
 		}
 	}
 
@@ -22,25 +25,31 @@ impl PointCloud {
 		unimplemented!();
 	}
 
-	pub fn insert(&mut self, p: Point) {
+	pub fn push(&mut self, p: Point) {
+		self.point_log.push_back(p);
 		for p in self.cover.insert(p).into_iter() {
-			self.interior.insert(p);
+			self.interior.insert(p.to_bits());
 		}
 	}
 
-	pub fn remove(&mut self, p: Point) {
-		if self.interior.remove(&p) {
-			return;
-		}
+	pub fn pop(&mut self) {
+		if let Some(p) = self.point_log.pop_front() {
+			if self.interior.remove::<(u64, u64)>(&p.to_bits()) {
+				return;
+			}
 
-		if let Some(i) = self.cover.find(p) {
-			let lost_area = ConvexPolygon::new();
-			lost_area.insert(self.cover.rev_edge(i).0);
-			lost_area.insert(self.cover.fwd_edge(i).1);
-			lost_area.insert(self.cover.remove(i));
+			if let Some(i) = self.cover.find(p) {
+				let mut lost_area = ConvexPolygon::new();
+				lost_area.insert(self.cover.rev_edge(i).0);
+				lost_area.insert(self.cover.fwd_edge(i).1);
+				lost_area.insert(self.cover.remove(i));
 
-			for p in self.interior.iter().filter(|p| lost_area.covers(p)) {
-				self.cover.insert(*p);
+				for p in self.interior.iter()
+					.map(|b| Point::from_bits(*b))
+					.filter(|p| lost_area.covers(*p))
+				{
+					self.cover.insert(p);
+				}
 			}
 		}
 	}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,7 +4,7 @@ use smallest_circle::points::Point;
 
 #[test]
 fn point_cloud_use() {
-	let mut point_cloud = smallest_cover::PointCloud::new();
+	let mut point_cloud = smallest_cover::MovingPointCloud::new();
 
 	// Populate initial points
 	{
@@ -26,40 +26,28 @@ fn point_cloud_use() {
 		let theta0 = 0_f64;
 		let r = 0.5;
 
-		point_cloud.insert(Point{x: r*theta0.cos(), y: r*theta0.sin()});
-		point_cloud.insert(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
-		point_cloud.insert(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
-		point_cloud.insert(Point{x: r*(theta0+3.*dth).cos(), y: r*(theta0+3.*dth).sin()});
+		point_cloud.push(Point{x: r*theta0.cos(), y: r*theta0.sin()});
+		point_cloud.push(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
+		point_cloud.push(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
+		point_cloud.push(Point{x: r*(theta0+3.*dth).cos(), y: r*(theta0+3.*dth).sin()});
 	};
 	assert!(point_cloud.cover_radius() == 1.);
 
 	// Remove original polygon vertices
-	{
-		let dth = 120_f64.to_radians();
-		let theta0 = 1_f64;
-		let r = 1.;
-		
-		point_cloud.remove(Point{x: r*theta0.cos(), y: r*theta0.sin()});
-		point_cloud.remove(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
-		point_cloud.remove(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
-	};
+	point_cloud.pop();
+	point_cloud.pop();
+	point_cloud.pop();
 	assert!(point_cloud.cover_radius() == 0.5);
 
 
 	// Add new polygon vertices, w/o changing radius
-	point_cloud.insert(Point{x: 0.3, y: 0.3});
+	point_cloud.push(Point{x: 0.3, y: 0.3});
 	assert!(point_cloud.cover_radius() == 0.5);
 
 	// Remove all but one point
-	{
-		let dth = 90_f64.to_radians();
-		let theta0 = 0_f64;
-		let r = 0.5;
-
-		point_cloud.remove(Point{x: r*theta0.cos(), y: r*theta0.sin()});
-		point_cloud.remove(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
-		point_cloud.remove(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
-		point_cloud.remove(Point{x: r*(theta0+3.*dth).cos(), y: r*(theta0+3.*dth).sin()});
-	};
+	point_cloud.pop();
+	point_cloud.pop();
+	point_cloud.pop();
+	point_cloud.pop();
 	assert!(point_cloud.cover_radius() == 0.);
 }
