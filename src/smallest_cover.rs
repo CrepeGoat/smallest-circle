7cce917783db8a95a72c98dee072ec313ae3ce9a
CrepeGoat <awqatty.b@gmail.com>
2020-02-02 03:36:27 -0500

    partially implemented `PointCloud` interface w/ `ConvexPolygon` interface

--- src/smallest_cover.rs
+++ src/smallest_cover.rs
@@ -1,12 +1,21 @@
 use crate::points::Point;
+use crate::polygon::ConvexPolygon;
 
 use std::iter::Iterator;
+use std::collections::HashSet;
 
-pub struct PointCloud{}
+
+pub struct PointCloud{
+	cover: ConvexPolygon,
+	interior: HashSet<Point>,
+}
 
 impl PointCloud {
 	pub fn new() -> Self {
-		Self{}
+		Self{
+			cover: ConvexPolygon::new(),
+			interior: HashSet::new(),
+		}
 	}
 
 	pub fn extend<I: Iterator<Item=Point>>(&mut self, iter: I) {
@@ -14,11 +23,26 @@ impl PointCloud {
 	}
 
 	pub fn insert(&mut self, p: Point) {
-		unimplemented!();
+		for p in self.cover.insert(p).into_iter() {
+			self.interior.insert(p);
+		}
 	}
 
 	pub fn remove(&mut self, p: Point) {
-		unimplemented!();
+		if self.interior.remove(&p) {
+			return;
+		}
+
+		if let Some(i) = self.cover.find(p) {
+			let lost_area = ConvexPolygon::new();
+			lost_area.insert(self.cover.rev_edge(i).0);
+			lost_area.insert(self.cover.fwd_edge(i).1);
+			lost_area.insert(self.cover.remove(i));
+
+			for p in self.interior.iter().filter(|p| lost_area.covers(p)) {
+				self.cover.insert(*p);
+			}
+		}
 	}
 
 	pub fn cover_radius(&self) -> f64 {
