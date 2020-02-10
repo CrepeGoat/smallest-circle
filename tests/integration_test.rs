9d93d6e71308930446086b7f4a5769ba6d17c665
CrepeGoat <awqatty.b@gmail.com>
2020-02-09 20:33:40 -0500

    rewrote integration tests using `Point` structs

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,15 +1,65 @@
 use smallest_circle::smallest_cover;
+use smallest_circle::points::Point;
+
 
 #[test]
 fn point_cloud_use() {
 	let mut point_cloud = smallest_cover::PointCloud::new();
 
-	point_cloud.extend(0u8..5u8);
-	assert_eq!(point_cloud.cover_radius(), 0.);  // replace w/ calculated value
+	// Populate initial points
+	{
+		let dth = 120_f64.to_radians();
+		let theta0 = 1_f64;
+		let r = 1.;
+		
+		point_cloud.extend(vec!(
+			Point{x: r*theta0.cos(), y: r*theta0.sin()},
+			Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()},
+			Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()},
+		).into_iter());
+	};
+	assert_eq!(point_cloud.cover_radius(), 1.);
 
-	for i in 5u8..10u8 {
-		point_cloud.insert(i);
-		point_cloud.remove(i-5);
-		assert_eq!(point_cloud.cover_radius(), 0.);  // replace w/ calculated value
-	}
+	// Add interior points
+	{
+		let dth = 90_f64.to_radians();
+		let theta0 = 0_f64;
+		let r = 0.5;
+
+		point_cloud.insert(Point{x: r*theta0.cos(), y: r*theta0.sin()});
+		point_cloud.insert(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
+		point_cloud.insert(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
+		point_cloud.insert(Point{x: r*(theta0+3.*dth).cos(), y: r*(theta0+3.*dth).sin()});
+	};
+	assert_eq!(point_cloud.cover_radius(), 1.);
+
+	// Remove original polygon vertices
+	{
+		let dth = 120_f64.to_radians();
+		let theta0 = 1_f64;
+		let r = 1.;
+		
+		point_cloud.remove(Point{x: r*theta0.cos(), y: r*theta0.sin()});
+		point_cloud.remove(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
+		point_cloud.remove(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
+	};
+	assert_eq!(point_cloud.cover_radius(), 0.5);
+
+
+	// Add new polygon vertices, w/o changing radius
+	point_cloud.insert(Point{x: 0.3, y: 0.3});
+	assert_eq!(point_cloud.cover_radius(), 0.5);
+
+	// Remove all but one point
+	{
+		let dth = 90_f64.to_radians();
+		let theta0 = 0_f64;
+		let r = 0.5;
+
+		point_cloud.remove(Point{x: r*theta0.cos(), y: r*theta0.sin()});
+		point_cloud.remove(Point{x: r*(theta0+dth).cos(), y: r*(theta0+dth).sin()});
+		point_cloud.remove(Point{x: r*(theta0+2.*dth).cos(), y: r*(theta0+2.*dth).sin()});
+		point_cloud.remove(Point{x: r*(theta0+3.*dth).cos(), y: r*(theta0+3.*dth).sin()});
+	};
+	assert_eq!(point_cloud.cover_radius(), 0.);
 }
