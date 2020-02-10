f0753be70e11ef9d8506011116f4ced31686644e
CrepeGoat <awqatty.b@gmail.com>
2020-02-09 20:54:47 -0500

    added `Points` struct
    - referenced in `PointCloud` & lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod smallest_cover;
+pub mod points;
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@ use smallest_circle::smallest_cover;
 
 #[test]
 fn point_cloud_use() {
-	let mut point_cloud = smallest_cover::PointCloud::<u8>::new();
+	let mut point_cloud = smallest_cover::PointCloud::new();
 
 	point_cloud.extend(0u8..5u8);
 	assert_eq!(point_cloud.cover_radius(), 0.);  // replace w/ calculated value
