f690fcf527decc27d97925346587bd32d802364f
CrepeGoat <awqatty.b@gmail.com>
2020-02-23 11:44:03 -0500

    partially implemented `MovingPointCloud.cover_radius`

--- src/smallest_cover.rs
+++ src/smallest_cover.rs
@@ -1,4 +1,4 @@
-use crate::points::Point;
+use crate::points::{Point, Vector};
 use crate::polygon::ConvexPolygon;
 
 use std::iter::Iterator;
@@ -58,6 +58,58 @@ impl MovingPointCloud {
 	}
 
 	pub fn cover_radius(&self) -> f64 {
+		if self.cover.degree() <= 1 {
+			return 0.
+		}
+
+		// Calculate square center
+		let center = {
+			let x_bounds = (
+				self.cover.find_best(Vector{x: -1., y: 0.}).position().x,
+				self.cover.find_best(Vector{x: 1., y: 0.}).position().x,
+			);
+			let y_bounds = (
+				self.cover.find_best(Vector{x: 0., y: -1.}).position().y,
+				self.cover.find_best(Vector{x: 0., y: 1.}).position().y,
+			);
+
+			Point{
+				x: (x_bounds.0+x_bounds.1) / 2.,
+				y: (y_bounds.0+y_bounds.1) / 2.,
+			}
+		};
+
+		let find_farthest_pos = |ignores: HashSet<(u64, u64)>| {
+			let mut vertex = self.cover.some_vertex();
+			let mut best_pos: Option<Point> = None;
+			
+			for _i in 0..self.cover.degree() {
+				let pos = vertex.position();
+				vertex = vertex.fwd_vertex();
+
+				if ignores.contains(&pos.to_bits()) {
+					continue;
+				}
+				best_pos = match best_pos {
+					None => Some(pos),
+					Some(b_pos) => Some({
+						if (pos-center).sq_mag() > (b_pos-center).sq_mag() {
+							pos
+						} else {
+							b_pos
+						}
+					}),
+				};
+			}
+
+			best_pos
+		};
+
+		let circ_pos1 = find_farthest_pos(HashSet::<(u64, u64)>::new()).unwrap();
+		let circ_pos2 = find_farthest_pos(
+			vec![circ_pos1].into_iter().map(|p| p.to_bits()).collect()
+		).unwrap();
+
 		unimplemented!();
 	}
 }
